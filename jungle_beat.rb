4dbcc1b165114114646b5ac922437e28890d55a5
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-12 11:01:15 -0600

    through 6

--- jungle_beat.rb
+++ jungle_beat.rb
@@ -2,17 +2,11 @@ require "./lib/jungle_beat"
 require 'pry'
 jb = JungleBeat.new
 # => <JungleBeat list=<LinkedList head=nil #234567890890> #456789045678>
-p jb.list
-# => <LinkedList head=nil #234567890890>
-p jb.list.head
-# => nil
-p jb.append("deep doo ditt")
-# => "deep doo ditt"
-p jb.list.head.data
-# => "deep"
-p jb.list.head.next_node.data
-# => "doo"
-p jb.append("woo hoo shu")
-# => "woo hoo shu"
+p jb.append("deep doo ditt woo hoo shu")
+# => "deep doo ditt woo hoo shu"
 p jb.count
-# 6
+# => 6
+p jb.list.count
+# => 6
+p jb.play
+# => # plays the sounds deep doo ditt woo hoo shu
